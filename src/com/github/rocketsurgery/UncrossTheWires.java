08c54c1e03b3b3cf35d007233507e5c7a024b645
excaliburHisSheath <excaliburHisSheath@gmail.com>
2012-12-26 16:18:03 -0600

    Adds Empty Game File Sets up UncrossTheWires.java to extend BasicGame Removes HelloWorld.java test file

--- src/com/github/rocketsurgery/UncrossTheWires.java
+++ src/com/github/rocketsurgery/UncrossTheWires.java
@@ -1,9 +1,30 @@
 package com.github.rocketsurgery;
 
-public class UncrossTheWires {
+import org.newdawn.slick.*;
+
+public class UncrossTheWires extends BasicGame {
 	
-	public static void main(String[] args) {
-		System.out.println("Hello World!");
+	public UncrossTheWires(String title) {
+		super(title);
+	}
+
+	@Override
+	public void render(GameContainer arg0, Graphics g) throws SlickException {
+	}
+
+	@Override
+	public void init(GameContainer arg0) throws SlickException {
+	}
+
+	@Override
+	public void update(GameContainer arg0, int arg1) throws SlickException {
+	}
+	
+	public static void main(String[] args) throws SlickException {
+		AppGameContainer app = new AppGameContainer(new UncrossTheWires("Uncross"));
+
+		app.setDisplayMode(800, 600, false);
+		app.start();
 	}
 
 }
